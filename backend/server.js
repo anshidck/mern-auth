2b69999da2cfc97476b9ab5d212ae761e022d191
Anshid ck <anshidck724@gmail.com>
2023-06-01 21:33:05 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const PORT = process.env.PORT || 5000
+const path = require('path')
 const goalRouter = require('./router/goalRouter');
 const userRouter = require('./router/userRouter')
 const connectDB = require('./config/db');
@@ -10,9 +11,22 @@ const app = express();
 connectDB();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use('/api/goals', goalRouter);
 app.use('/api/users', userRouter);
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+    app.get('*', (req, res) =>
+        res.sendFile(
+            path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+        )
+    );
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+}
 
 app.listen(PORT, () => {
     console.log(`server started ${PORT}`)
